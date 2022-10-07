3f5a5993a9d1d588939b6296d89381f18c19883a
VishalGaud17 <112686929+VishalGaud17@users.noreply.github.com>
2022-10-07 12:05:17 +0530

    Update Area.java

--- Practical/Interface/Area.java
+++ Practical/Interface/Area.java
@@ -1,10 +1,13 @@
 package Practical;
 
+//Abstract Method of Interface
 interface Area {
 	void getArea(int length,int breadth);
 }
+//Implement the Area Interface
 class Rectangle implements Area{
-
+	
+	//Implementation of abstract Method
 	public void getArea(int length, int breadth) {
 		System.out.println("The area of the Rectangle is "+(length * breadth));
 		
