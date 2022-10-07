ee2f7fc03ce7a3a4c823df201c2334814428f8ef
VishalGaud17 <112686929+VishalGaud17@users.noreply.github.com>
2022-10-07 12:02:34 +0530

    Update Area.java

--- Practical/Interface/Area.java
+++ Practical/Interface/Area.java
@@ -3,10 +3,8 @@ package Practical;
 interface Area {
 	void getArea(int length,int breadth);
 }
-
 class Rectangle implements Area{
 
-	@Override
 	public void getArea(int length, int breadth) {
 		System.out.println("The area of the Rectangle is "+(length * breadth));
 		
