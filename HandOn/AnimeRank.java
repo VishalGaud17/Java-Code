658548515205249ef8e2f4903d2af51ce8ddeb81
VishalGaud17 <112686929+VishalGaud17@users.noreply.github.com>
2022-10-10 16:34:02 +0530

    Add files via upload

---
+++ HandOn/AnimeRank.java
@@ -0,0 +1,23 @@
+package Anime;
+
+public class AnimeRank {
+	String Name;
+	int Rank;
+	String Protagonist;
+	int Ep;
+	String Antagonist;
+	public AnimeRank(String a,int b,int c,String d,String e) {
+		this.Name=a;
+		this.Rank=b;
+		this.Ep=c;
+		this.Protagonist=d;
+		this.Antagonist=e;
+	}
+	/*public AnimeRank(String name2, int rank2, int ep2, String protagonist2, String antagonist2) {
+		this.Name=name2;
+		this.Rank=rank2;
+		this.Ep=ep2;
+		this.Protagonist=protagonist2;
+		this.Antagonist=antagonist2;
+	}*/
+}
