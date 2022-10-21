098cf66d84b1d0982361c419d67c4f3ae2cc92cd
VishalGaud17 <112686929+VishalGaud17@users.noreply.github.com>
2022-10-21 13:38:08 +0530

    Add files via upload

---
+++ Practical/Array/ArrayList1.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.*;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		System.out.println();
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("Mazda");
+		System.out.println(cars);
+
+	}
+
+}
